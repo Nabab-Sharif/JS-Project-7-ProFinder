45620c1832b80acf4fbbdc927124e0a25099a0f8
md nabab sharif <mdnababsharifdiu@gmail.com>
2023-09-25 15:06:58 +0600

    Search & Show Section Done

--- index.js
+++ index.js
@@ -18,9 +18,44 @@ document.querySelector('#search').addEventListener('submit', async (e) => {
   e.preventDefault();
   const username = document.querySelector('#findByUsername').value;
   const profile = await getUser(username);
-  console.log(profile)
+
+  showProfile(profile)
 })
 //..........................Get Information from API End Here..................................
 
 
 
+
+
+
+//..........................Search & Show Start Here..................................
+
+function showProfile(profile) {
+  document.querySelector('.profile').innerHTML = `
+  <img src="${profile.avatar_url}"
+  alt="${profile.name}" />
+  <p class="name">${profile.name}</p>
+  <p class="username login">${profile.login}</p>
+  <p class="bio">
+   ${profile.bio}</p>
+
+  <div class="followers-stars">
+    <p>
+      <ion-icon name="people-outline"></ion-icon>
+      <span class="followers"> ${profile.followers} </span> followers
+    </p>
+    <span class="dot">·</span>
+    <p><span class="following"> ${profile.following} </span> following</p>
+  </div>
+
+  <p class="company">
+    <ion-icon name="business-outline"></ion-icon>
+    ${profile.company}
+  </p>
+  <p class="location">
+    <ion-icon name="location-outline"></ion-icon>${profile.location}
+  </p>
+  `
+}
+
+//..........................Search & Show End Here..................................
